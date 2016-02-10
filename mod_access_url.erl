771a1c0e0cc184f21f60882c5ce5d10604a1205e
Marc Worrell <marc@worrell.nl>
2016-02-10 14:16:57 +0100

    Fix for signed page-images. Also convert tabs to spaces.

--- mod_access_url.erl
+++ mod_access_url.erl
@@ -25,180 +25,179 @@
 -mod_description("Sign an URL for an user so that it can be used for accessing vcal feeds etc.").
 
 -export([
-	observe_url_rewrite/3,
-	observe_request_context/3
-	]).
+    observe_url_rewrite/3,
+    observe_request_context/3
+    ]).
 
 -include_lib("zotonic.hrl").
 
 observe_url_rewrite(#url_rewrite{dispatch=image, args=Args}, Url, Context) ->
-	case z_context:get(is_z_access_url, Context) of
-		true ->
-			maybe_add_token(image, Args, Url, Context);
-		_False ->
-			rewrite_if_access_url(image, Args, Url, Context)
-	end;
+    case z_context:get(is_z_access_url, Context) of
+        true ->
+            maybe_add_token(image, Args, Url, Context);
+        _False ->
+            rewrite_if_access_url(image, Args, Url, Context)
+    end;
 observe_url_rewrite(#url_rewrite{dispatch=Dispatch, args=Args}, Url, Context) ->
     rewrite_if_access_url(Dispatch, Args, Url, Context).
 
 rewrite_if_access_url(Dispatch, Args, Url, Context) ->
-	case proplists:get_value(z_access_url, Args) of
-		true ->
-			maybe_add_token(Dispatch, Args, Url, Context);
-		UserId when is_integer(UserId) ->
-			maybe_add_token(Dispatch, Args, Url, z_acl:logon(UserId, Context));
-		_ ->
-			Url
-	end.
+    case proplists:get_value(z_access_url, Args) of
+        true ->
+            maybe_add_token(Dispatch, Args, Url, Context);
+        UserId when is_integer(UserId) ->
+            maybe_add_token(Dispatch, Args, Url, z_acl:logon(UserId, Context));
+        _ ->
+            Url
+    end.
 
 observe_request_context(request_context, Context, _Context) ->
-	case z_convert:to_bool(z_context:get_q("z_access_url", Context)) of
-		true ->
-			logon_if_sigok(Context);
-		false ->
-			Context
-	end.
+    case z_context:get_q("z_access_url_token", Context) of
+        undefined -> Context;
+        "" -> Context;
+        _ -> logon_if_sigok(Context)
+    end.
 
 maybe_add_token(Dispatch, Args, Url, Context) ->
-	case z_acl:user(Context) of
-		undefined ->
-			Url;
-		UserId when is_integer(UserId) ->
-			{ok, Token, Secret} = user_secret(UserId, Context),
-			Nonce = z_convert:to_binary(z_ids:id()),
-			Sig = sign(Dispatch, Args, Token, Nonce, Secret),
-			Sig1 = z_convert:to_binary(z_utils:url_encode(Sig)),
-			ExtraArgs = <<"z_access_url_token=", Token/binary,
-						  "&z_access_url_nonce=", Nonce/binary,
-						  "&z_access_url_sig=", Sig1/binary>>,
-			case binary:match(Url, <<"?">>) of
-				nomatch ->
-					<<Url/binary, $?, ExtraArgs/binary>>;
-				_ ->
-					<<Url/binary, $&, ExtraArgs/binary>>
-			end
-	end.
+    case z_acl:user(Context) of
+        undefined ->
+            Url;
+        UserId when is_integer(UserId) ->
+            {ok, Token, Secret} = user_secret(UserId, Context),
+            Nonce = z_convert:to_binary(z_ids:id()),
+            Sig = sign(Dispatch, Args, Token, Nonce, Secret),
+            Sig1 = z_convert:to_binary(z_utils:url_encode(Sig)),
+            ExtraArgs = <<"z_access_url_token=", Token/binary,
+                          "&z_access_url_nonce=", Nonce/binary,
+                          "&z_access_url_sig=", Sig1/binary>>,
+            case binary:match(Url, <<"?">>) of
+                nomatch ->
+                    <<Url/binary, $?, ExtraArgs/binary>>;
+                _ ->
+                    <<Url/binary, $&, ExtraArgs/binary>>
+            end
+    end.
 
 user_secret(UserId, Context) ->
-	case m_identity:get_rsc_by_type(UserId, ?MODULE, Context) of
-		[] ->
-			Token = z_convert:to_binary(z_ids:id(20)),
-			Secret = z_convert:to_binary(z_ids:id(40)),
-			m_identity:insert(UserId, ?MODULE, Token, [{prop1, Secret}], Context),
-			{ok, Token, Secret};
-		[Idn|_] ->
-			Token = proplists:get_value(key, Idn),
-			Secret = proplists:get_value(prop1, Idn),
-			{ok, Token, Secret} 
-	end.
+    case m_identity:get_rsc_by_type(UserId, ?MODULE, Context) of
+        [] ->
+            Token = z_convert:to_binary(z_ids:id(20)),
+            Secret = z_convert:to_binary(z_ids:id(40)),
+            m_identity:insert(UserId, ?MODULE, Token, [{prop1, Secret}], Context),
+            {ok, Token, Secret};
+        [Idn|_] ->
+            Token = proplists:get_value(key, Idn),
+            Secret = proplists:get_value(prop1, Idn),
+            {ok, Token, Secret} 
+    end.
 
 token_user(Token, Context) ->
-	case m_identity:lookup_by_type_and_key(?MODULE, Token, Context) of
-		undefined ->
-			{error, enoent};
-		Idn when is_list(Idn) ->
-			UserId = proplists:get_value(rsc_id, Idn),
-			Secret = proplists:get_value(prop1, Idn),  
-			{ok, UserId, Secret}
-	end.
+    case m_identity:lookup_by_type_and_key(?MODULE, Token, Context) of
+        undefined ->
+            {error, enoent};
+        Idn when is_list(Idn) ->
+            UserId = proplists:get_value(rsc_id, Idn),
+            Secret = proplists:get_value(prop1, Idn),  
+            {ok, UserId, Secret}
+    end.
 
 logon_if_sigok(Context) ->
-	Token = z_convert:to_binary(z_context:get_q(z_access_url_token, Context)),
-	case token_user(Token, Context) of
-		{ok, UserId, Secret} ->
-			Nonce = z_context:get_q(z_access_url_nonce, Context),
-			Dispatch = z_context:get_q(zotonic_dispatch, Context),
-			Sig = z_convert:to_binary(z_context:get_q(z_access_url_sig, Context)),
-			case is_valid_signature(Sig, Dispatch, get_q_all(Context), Token, Nonce, Secret) of
-				true ->
-					Context1 = z_context:set(is_z_access_url, true, Context),
-					z_context:set_noindex_header(true, z_acl:logon(UserId, Context1));
-				false ->
-					lager:warning("Non matching signature on request ~p", [wrq:raw_path(z_context:get_reqdata(Context))]),
-					Context
-			end;
-		{error, enoent} ->
-			lager:info("Unknown url_access_token \"~p\"", [Token]),
-			Context
-	end.
+    Token = z_convert:to_binary(z_context:get_q(z_access_url_token, Context)),
+    case token_user(Token, Context) of
+        {ok, UserId, Secret} ->
+            Nonce = z_context:get_q(z_access_url_nonce, Context),
+            Dispatch = z_context:get_q(zotonic_dispatch, Context),
+            Sig = z_convert:to_binary(z_context:get_q(z_access_url_sig, Context)),
+            case is_valid_signature(Sig, Dispatch, get_q_all(Context), Token, Nonce, Secret) of
+                true ->
+                    Context1 = z_context:set(is_z_access_url, true, Context),
+                    z_context:set_noindex_header(true, z_acl:logon(UserId, Context1));
+                false ->
+                    lager:warning("Non matching signature on request ~p", [wrq:raw_path(z_context:get_reqdata(Context))]),
+                    Context
+            end;
+        {error, enoent} ->
+            lager:info("Unknown url_access_token \"~p\"", [Token]),
+            Context
+    end.
 
 is_valid_signature(Sig, Dispatch, Args, Token, Nonce, Secret) ->
-	ArgsFiltered = filter_args(Args, []),
-	Sig1 = fix_outlook_sig(Sig),
-	case sign_args(Dispatch, ArgsFiltered, Token, Nonce, Secret) of
-		Sig ->
-			true;
-		Sig1 ->
-			true;
-		_NonMatchSig ->
-			% For a short time there was a version where use_absolute_url
-			% was not filtered from the signature generation.
-			% Retry with the use_absolute_url argument to check for these
-			% wrongly generated signatures.
-			ArgsFiltered1 = lists:sort([{<<"use_absolute_url">>, <<"true">>}|ArgsFiltered]),
-			sign_args(Dispatch, ArgsFiltered1, Token, Nonce, Secret) =:= Sig
-	end.
+    ArgsFiltered = filter_args(Args, []),
+    Sig1 = fix_outlook_sig(Sig),
+    case sign_args(Dispatch, ArgsFiltered, Token, Nonce, Secret) of
+        Sig ->
+            true;
+        Sig1 ->
+            true;
+        _NonMatchSig ->
+            % For a short time there was a version where use_absolute_url
+            % was not filtered from the signature generation.
+            % Retry with the use_absolute_url argument to check for these
+            % wrongly generated signatures.
+            ArgsFiltered1 = lists:sort([{<<"use_absolute_url">>, <<"true">>}|ArgsFiltered]),
+            sign_args(Dispatch, ArgsFiltered1, Token, Nonce, Secret) =:= Sig
+    end.
 
 
 sign(Dispatch, Args, Token, Nonce, Secret) ->
-	sign_args(Dispatch, filter_args(Args, []), Token, Nonce, Secret).
+    sign_args(Dispatch, filter_args(Args, []), Token, Nonce, Secret).
 
 sign_args(Dispatch, Args, Token, Nonce, Secret) ->
-	Data = term_to_binary([
-					Args,
-					signed, 
-					z_convert:to_binary(Dispatch),
-					z_convert:to_binary(Nonce),
-					z_convert:to_binary(Token),
-					z_convert:to_binary(Secret)
-				]),
+    Data = term_to_binary([
+                    Args,
+                    signed, 
+                    z_convert:to_binary(Dispatch),
+                    z_convert:to_binary(Nonce),
+                    z_convert:to_binary(Token),
+                    z_convert:to_binary(Secret)
+                ]),
      base64:encode(crypto:hash(sha256, Data)).
 
 get_q_all(Context) ->
-	Args = z_context:get_q_all_noz(Context),
-	case wrq:disp_path(z_context:get_reqdata(Context)) of
-		[] ->
-			Args;
-		Path ->
-			[{star,Path}|Args]
-	end.
+    Args = z_context:get_q_all_noz(Context),
+    case wrq:disp_path(z_context:get_reqdata(Context)) of
+        [] ->
+            Args;
+        Path ->
+            [{star,Path}|Args]
+    end.
 
 filter_args([], Acc) ->
-	lists:sort(Acc);
+    lists:sort(Acc);
 filter_args([Token|Args], Acc) when is_atom(Token) ->
-	filter_args([{Token,<<"true">>}|Args], Acc);
+    filter_args([{Token,<<"true">>}|Args], Acc);
 filter_args([{"z_access_url"++_,_}|Args], Acc) ->
-	filter_args(Args, Acc);
+    filter_args(Args, Acc);
 filter_args([{<<"z_access_url", _/binary>>,_}|Args], Acc) ->
-	filter_args(Args, Acc);
+    filter_args(Args, Acc);
 filter_args([{"z_language",_}|Args], Acc) ->
-	filter_args(Args, Acc);
+    filter_args(Args, Acc);
 filter_args([{"zotonic_"++_,_}|Args], Acc) ->
-	filter_args(Args, Acc);
+    filter_args(Args, Acc);
 filter_args([{<<"z_language">>,_}|Args], Acc) ->
-	filter_args(Args, Acc);
+    filter_args(Args, Acc);
 filter_args([{<<"zotonic_", _/binary>>,_}|Args], Acc) ->
-	filter_args(Args, Acc);
+    filter_args(Args, Acc);
 filter_args([{use_absolute_url,_}|Args], Acc) ->
-	filter_args(Args, Acc);
+    filter_args(Args, Acc);
 filter_args([{z_language,_}|Args], Acc) ->
-	filter_args(Args, Acc);
+    filter_args(Args, Acc);
 filter_args([{zotonic_dispatch,_}|Args], Acc) ->
-	filter_args(Args, Acc);
+    filter_args(Args, Acc);
 filter_args([{zotonic_dispatch_path,_}|Args], Acc) ->
-	filter_args(Args, Acc);
+    filter_args(Args, Acc);
 filter_args([{zotonic_dispatch_path_rewrite,_}|Args], Acc) ->
-	filter_args(Args, Acc);
+    filter_args(Args, Acc);
 filter_args([{K,V}|Args], Acc) ->
-	K1 = z_convert:to_binary(K),
-	case K1 of
-		<<"z_access_url", _/binary>> ->
-			filter_args(Args, Acc);
-		_ ->
-			V1 = z_convert:to_binary(V), 
-			filter_args(Args, [{K1,V1}|Acc])
-	end.
+    K1 = z_convert:to_binary(K),
+    case K1 of
+        <<"z_access_url", _/binary>> ->
+            filter_args(Args, Acc);
+        _ ->
+            V1 = z_convert:to_binary(V), 
+            filter_args(Args, [{K1,V1}|Acc])
+    end.
 
 %% Outlook.com decodes en re-combines urls, without percent-encoding.
 fix_outlook_sig(Sig) ->
-	binary:replace(Sig, <<" ">>, <<"+">>).
+    binary:replace(Sig, <<" ">>, <<"+">>).
